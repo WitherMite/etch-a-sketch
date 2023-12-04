b4a2dc177172819753868cbf9b9fd997bcb6a3b8
WitherMite <43625516+WitherMite@users.noreply.github.com>
2023-12-04 05:36:55 -0600

    Make rainbow pen work

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,8 @@
 const SCREEN = document.querySelector(".etchContainer");
 const SIZE_BTN = document.querySelector(".sideLengthBtn");
 const RESET_BTN = document.querySelector(".resetBtn");
+const RAINBOW_BTN = document.querySelector(".rainbowBtn");
+const BLACK_BTN = document.querySelector(".blackBtn");
 let currentPen = "black";
 
 fillScreen(16);
@@ -13,9 +15,19 @@ RESET_BTN.addEventListener("click", () => {
     const pixels = document.querySelectorAll(".pixel");
 
     pixels.forEach((pixel) => {
-        pixel.style.backgroundColor = "white";
+        pixel.style.backgroundColor = "rgb(255,255,255)";
         pixel.style.border = "1px solid black";
     });
+});
+
+RAINBOW_BTN.addEventListener("click", () => {
+    currentPen = "rainbow";
+    addMouseEnterBehavior();
+});
+
+BLACK_BTN.addEventListener("click", () => {
+    currentPen = "black";
+    addMouseEnterBehavior();
 });
 
 function fillScreen(sideLength) {
@@ -35,6 +47,7 @@ function fillScreen(sideLength) {
         PIXEL.setAttribute("style", 
             `height: ${pixelSize}%;
             flex: 1 1 ${pixelSize}%;
+            background-color: rgb(255,255,255);
             border: 1px solid black;`
         );
         PIXEL.classList.add('pixel');
@@ -56,6 +69,8 @@ function addMouseEnterBehavior() {
             addBlackMouseEnter();
             break;
         case "rainbow":
+            addRandomMouseEnter()
+            break;
         case "gradient":
     }
 }
@@ -65,7 +80,18 @@ function addBlackMouseEnter() {
 
     pixels.forEach((pixel) => {
         pixel.addEventListener("mouseenter", () => {
-            pixel.style.backgroundColor = "black";
+            pixel.style.backgroundColor = "rgb(0,0,0)";
+            pixel.style.border = "";
+        });
+    });
+}
+
+function addRandomMouseEnter() {
+    const pixels = document.querySelectorAll(".pixel");
+
+    pixels.forEach((pixel) => {
+        pixel.addEventListener("mouseenter", () => {
+            pixel.style.backgroundColor = `rgb(${getrandomColor()},${getrandomColor()},${getrandomColor()})`;
             pixel.style.border = "";
         });
     });
@@ -77,4 +103,8 @@ function getSideLength() {
     if (userInput && userInput <= 100) {
         return userInput;
     } else alert("You must enter a number between 1 and 100");
+}
+
+function getrandomColor() {
+    return Math.floor(Math.random() * 255);
 }
